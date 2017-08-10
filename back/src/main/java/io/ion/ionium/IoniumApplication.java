7f3e5359a9547f6cd81ac1a66e124311d1df1e5d
johannes.durandt <jrdurandt@mail.com>
2017-08-10 10:35:53 +0200

    Added basic backend Spring requirements

--- back/src/main/java/io/ion/ionium/IoniumApplication.java
+++ back/src/main/java/io/ion/ionium/IoniumApplication.java
@@ -1,4 +1,11 @@
 package io.ion.ionium;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class IoniumApplication {
+    public static void main(String[] args){
+        SpringApplication.run(IoniumApplication.class, args);
+    }
 }
